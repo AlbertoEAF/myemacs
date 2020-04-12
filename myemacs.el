e92050996b897e42e4aadf1e88aa3e164a827420
alberto.ferreira <alberto.ferreira@feedzai.com>
2020-04-12 11:47:53 +0100

    Bring back helm kill-ring and lispy from old configs

--- myemacs.el
+++ myemacs.el
@@ -69,17 +69,6 @@
 ;;     (doxymacs-mode t)
 ;;     (doxymacs-font-lock)))
 
-(use-package helm-config :ensure t)
-
-
-;;(helm-mode 1)
-
-;;(global-set-key (kbd "M-x") 'helm-M-x)
-
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-;;(global-set-key (kbd "C-x b") 'helm-buffers-list)
-(global-set-key (kbd "C-x b") 'helm-mini)
-;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 
 
